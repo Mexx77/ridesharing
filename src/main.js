068d3ffbd9ee0088028a74507777acf20e01f35c
Max Berndt <mb@maxberndt.com>
2019-08-22 15:45:56 +0200

    don't set apikey

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false;
 
 Vue.prototype.$http = axios;
 // read only api key
-axios.defaults.headers.common['x-apikey'] = '5d5d7d77a592085130522576';
+// axios.defaults.headers.common['x-apikey'] = '5d5d7d77a592085130522576';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 new Vue({
